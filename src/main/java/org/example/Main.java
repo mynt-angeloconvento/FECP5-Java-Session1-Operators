73b5cbfd324578c1f32629d7b370ec09f86b973b
Godfrey Angelo Convento <godfrey-a.convento@GFITFTLAPT03445.ad.mynt.xyz>
2025-06-30 14:47:19 +0800

    feat: add correct solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
-import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = myObj.nextDouble();
+        double fahrenheit = (celsius * 9 / 5 ) + 32;
+        System.out.print("Temperature in Fahrenheit: ");
+        System.out.printf("%.2f", fahrenheit);
     }
+
 }
